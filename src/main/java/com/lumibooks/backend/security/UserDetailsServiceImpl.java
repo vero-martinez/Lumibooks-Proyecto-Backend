3c72aa2ae887a9982f89a1303129fd189d1ff100
vero-martinez <veris.14@hotmail.com>
2026-05-11 20:14:38 -0500

    feat: agregar roles de usuario en la autenticación de Spring Security

--- src/main/java/com/lumibooks/backend/security/UserDetailsServiceImpl.java
+++ src/main/java/com/lumibooks/backend/security/UserDetailsServiceImpl.java
@@ -34,7 +34,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         return org.springframework.security.core.userdetails.User.builder() // Convertir la entidad User a un objeto UserDetails
                 .username(user.getEmail())
                 .password(user.getPassword())
-                .authorities(java.util.Collections.emptyList()) // Aquí se podrían agregar roles o permisos si se implementan
+                .authorities("ROLE_" + user.getRol().name()) // Aquí se podrían agregar roles o permisos si se implementan
                 .accountLocked(!user.getActivo()) // Si el campo "activo" es false, la cuenta se considera bloqueada
                 .build();
     }
