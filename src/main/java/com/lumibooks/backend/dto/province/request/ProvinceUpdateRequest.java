2013308175ac7be0aee91b4f9c08e37480ddce7b
vero-martinez <veris.14@hotmail.com>
2026-05-29 22:10:20 -0500

    feat: agregar ProvinceUpdateRequest DTO para la edición de una provincia

---
+++ src/main/java/com/lumibooks/backend/dto/province/request/ProvinceUpdateRequest.java
@@ -0,0 +1,25 @@
+package com.lumibooks.backend.dto.province.request;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * DTO para la actualización de una provincia existente.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProvinceUpdateRequest {
+
+    @Size(max = 150, message = "El nombre no puede superar los 150 caracteres")
+    private String name;
+
+    private Long departmentId;
+
+    private Boolean isActive;
+
+}
