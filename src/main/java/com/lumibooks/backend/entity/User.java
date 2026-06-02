a3ea14c1a4a9bd9bc3ac99cfa37dc90639d8a66f
vero-martinez <veris.14@hotmail.com>
2026-06-01 20:24:31 -0500

    feat: cambiar isActive de Boolean a boolean primitivo

--- src/main/java/com/lumibooks/backend/entity/User.java
+++ src/main/java/com/lumibooks/backend/entity/User.java
@@ -51,7 +51,7 @@ public class User {
     
     @Builder.Default
     @Column(name = "is_active", nullable = false)
-    private Boolean isActive = true;
+    private boolean isActive = true;
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
