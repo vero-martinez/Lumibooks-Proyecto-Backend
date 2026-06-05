a5e9364607c3d6a4a2cca1a98b2f445243e6621c
vero-martinez <veris.14@hotmail.com>
2026-06-05 10:14:44 -0500

    refactor: unificar DTOs de creación y actualización de departamento

--- src/main/java/com/lumibooks/backend/dto/department/request/DepartmentRequest.java
+++ src/main/java/com/lumibooks/backend/dto/department/request/DepartmentRequest.java
@@ -1,17 +1,22 @@
 package com.lumibooks.backend.dto.department.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * DTO para la creación y actualización de un departamento.
+ */
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class DepartmentUpdateRequest {
+public class DepartmentRequest {
 
+    @NotBlank(message = "El nombre es obligatorio")
     @Size(max = 150, message = "El nombre no puede superar los 150 caracteres")
     private String name;
 
