1638902a9798e18dd03340dd6daafa9b6a8a8479
vero-martinez <veris.14@hotmail.com>
2026-06-08 16:01:45 -0500

    feat: agregar BookResponse DTO que representa la información a detalle de un libro al ser creado o editado

---
+++ src/main/java/com/lumibooks/backend/dto/response/BookResponse.java
@@ -0,0 +1,33 @@
+package com.lumibooks.backend.dto.response;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.lumibooks.backend.enums.BookFormat;
+import com.lumibooks.backend.enums.BookLanguage;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * DTO que representa la información a detalle de un libro al ser creado o editado
+ */
+@Getter
+@Builder
+public class BookResponse {
+
+    private Long id;
+    private String coverImageUrl;
+    private String title;
+    private List<AuthorPublicResponse> authors;
+    private String description;
+    private BigDecimal price;
+    private String isbn;
+    private Integer pageCount;
+    private String publisherName;
+    private BookLanguage language;
+    private BookFormat format;
+    private Short editionYear;
+    private List<String> categories;
+
+}
