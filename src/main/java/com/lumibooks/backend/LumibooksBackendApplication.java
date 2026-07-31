0367ecb5f8546f2f20698379fd0701dceba78170
vero-martinez <veris.14@hotmail.com>
2026-07-31 18:48:37 -0500

    feat: habilitar la programación de tareas con @EnableScheduling

--- src/main/java/com/lumibooks/backend/LumibooksBackendApplication.java
+++ src/main/java/com/lumibooks/backend/LumibooksBackendApplication.java
@@ -2,8 +2,10 @@ package com.lumibooks.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LumibooksBackendApplication {
 
 	public static void main(String[] args) {
